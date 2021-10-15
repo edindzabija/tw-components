ae922e071785147f82920cd3c09f55965ca390cb
Edin Dzabija <edindzabija@gmail.com>
2021-10-16 01:21:33 +0200

    basic footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { Footer } from './components/footer/Footer/Footer';
 import Navbar from './components/navigation/Navbar/Navbar';
 
 function App() {
   return (
     <div>
       <Navbar />
+      
+      <Footer />
     </div>
   );
 }
